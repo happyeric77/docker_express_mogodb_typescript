f0ef62f2a297072e6a441258adc2ce3d2865c0ae
happyeric77 <happyeric77@gmail.com>
2022-04-10 15:29:56 +0900

    Init pro ject with basic typescript express template (without backend database)

---
+++ app/src/server.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import path from "path";
+import fs from "fs";
+import bodyParser from "body-parser";
+let app = express();
+
+// Render static (CSS & JS) files from root folder
+app.use(express.static(__dirname));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/profile-picture", function (req, res) {
+  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  res.writeHead(200, { "Content-Type": "image/jpg" });
+  res.end(img, "binary");
+});
+
+app.post("/update-profile", function (req, res) {
+  let userObj = req.body;
+  console.log("WIP: update-profile");
+  res.send("WIP: update-profile");
+});
+
+app.get("/get-profile", function (req, res) {
+  let response: any = {};
+  console.log("WIP: get-profile");
+
+  res.send("WIP: get-profile");
+});
+
+app.listen(3000, function () {
+  console.log("app listening on port 3000!");
+});
